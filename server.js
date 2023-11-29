4c39fac8db4ef232d078660aa577626d35d2c7da
kingstockton@gmail.com <>
2023-11-29 12:57:54 -0700

    Update bot name.

--- server.js
+++ server.js
@@ -2,33 +2,34 @@ const express = require("express");
 const path = require("path");
 const socketio = require("socket.io");
 const app = express();
-
+cosnt formatMessage = require('./utils/messages')
 const http = require("http");
+const formatMessage = require("./utils/messages");
 const server = http.createServer(app);
 const io = socketio(server);
 
 // set static folder
 app.use(express.static(path.join(__dirname, "public")));
-
+const botName = 'Chat Bot'
 // run when client connects to server
 
 io.on("connection", (socket) => {
   // Welcome current user (single)
-  socket.emit("message", "Welcome to ChatRoom");
+  socket.emit("message", formatMessage(botName, "Welcome to ChatRoom"));
 
   // Broadcast to everyone else when a user connects
-  socket.broadcast.emit("message", " a  user has joined the chat");
+  socket.broadcast.emit("message",formatMessage(botName, "A user has joined the chat"));
 
   // Broadcast to everyone
   io.emit();
 
   socket.on("disconnect", () => {
-    io.emit("message", "a user has left the chat");
+    io.emit("message",formatMessage(botName, "A user has left the chat"));
   });
 
   // Listen for chatMessage
   socket.on("chat-message", (msg) => {
-    io.emit("message", msg);
+    io.emit("message", formatMessage("USER", msg));
   });
 });
 
